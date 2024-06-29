84106e670b92d957f08cb89712f398e616b4b4d3
Lasse Pehkonen <lasse.p.pehkonen@gmail.com>
2024-06-29 20:29:14 +0300

    first commit

---
+++ src/collections/Documents.js
@@ -0,0 +1,11 @@
+export const Documents = {
+    slug: 'documents',
+    fields: [
+      {
+        name: 'name',
+        type: 'text',
+        label: 'Name',
+        required: true,
+      },
+    ],
+  };
---
+++ src/payload.config.ts
@@ -0,0 +1,53 @@
+// storage-adapter-import-placeholder
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { buildConfig } from 'payload'
+import { fileURLToPath } from 'url'
+import sharp from 'sharp'
+
+import { Users } from './collections/Users'
+import { Media } from './collections/Media'
+import { Documents } from './collections/Documents'
+import { s3Storage } from '@payloadcms/storage-s3'
+
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+  },
+  collections: [Users, Documents, Media],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI || '',
+    },
+  }),
+  sharp,
+  plugins: [
+    s3Storage({
+      collections: {
+        media: {
+          prefix: 'media',
+        }
+      },
+      bucket: process.env.S3_BUCKET,
+      config: {
+        forcePathStyle: true, // Important for using Supabase
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        },
+        region: process.env.S3_REGION,
+        endpoint: process.env.S3_ENDPOINT,
+      },
+    }),
+  ],
+})
